571b7022f8311e1421359ecd3b3641b4029756fa
lakshay1906 <lbhati020@gmail.com>
2026-05-14 14:32:03 +0530

    debugging cors issue

--- components/frontendcomponents/pages/career/index.jsx
+++ components/frontendcomponents/pages/career/index.jsx
@@ -16,6 +16,7 @@ import { useGetAllCareersQuery } from "@/store/frontendSlice/frontendAPISlice";
 export default function CareerPage() {
   const { setSections } = useSideNav();
   const { data: careersApiResponse, isLoading } = useGetAllCareersQuery();
+  console.log(careersApiResponse, "careersApiResponse");
   const activeCareers = (careersApiResponse?.data || []).filter(
     (career) => Number(career.ActiveStatus) === 1,
   );
